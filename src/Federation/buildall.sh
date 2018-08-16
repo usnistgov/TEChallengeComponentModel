4da8aef96a8826dde938553a288b2ccc50cb6ef9
MartyBurns <marty@hypertek.us>
2018-08-16 15:22:31 -0400

    buildall master script

---
+++ src/Federation/buildall.sh
@@ -0,0 +1,26 @@
+cd Grid_generated
+mvn clean install
+cd ..
+
+cd GridLAB-D_generated
+mvn clean install
+cd ..
+cd Loads_generated
+mvn clean install
+cd ..
+cd LocalController_generated
+mvn clean install
+cd ..
+cd supervisoryController_generated
+mvn clean install
+cd ..
+cd TransactiveAgent_generated
+mvn clean install
+cd ..
+cd TEChallenge_generated
+mvn clean install
+cd ..
+cd TEChallenge_deployment
+mvn clean install
+cd ..
+
