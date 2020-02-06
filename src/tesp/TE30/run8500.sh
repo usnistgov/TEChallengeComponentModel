c322117f0c7bae56b96e6c15d5052f74b99eb29d
Thomas Roth <thomas.roth@nist.gov>
2020-02-06 22:35:19 +0000

    IEEE8500 federation (crashes on run)

--- src/tesp/TE30/run8500.sh
+++ src/tesp/TE30/run8500.sh
@@ -84,7 +84,7 @@ xterm -fg cyan -bg black -l -lf $logs_directory/pypower-${timestamp}.log -T "PyP
 waitUntilJoined PyPower 1
 
 cd $root_directory/TE30_generated/Grid30
-xterm -fg white -bg black -l -lf $logs_directory/gridlabd-${timestamp}.log -T "GridLAB-D" -geometry 140x40+720+240 -e "sh run.sh" &
+xterm -fg white -bg black -l -lf $logs_directory/gridlabd-${timestamp}.log -T "GridLAB-D" -geometry 140x40+720+240 -e "sh run8500.sh" &
 
 # terminate the simulation
 read -n 1 -r -s -p "Press any key to terminate the federation execution..."
