8335b61da62ed212a02b40240adb8af417c0a2e1
Thomas Roth <thomas.roth@nist.gov>
2020-05-14 06:08:58 +0000

    setup hla/parity hooks

---
+++ src/tesp/IEEE8500/parity/ParityMarket_generated/ParitySystem/src/main/java/gov/nist/hla/parity/paritysystem/OrderBooks.java
@@ -0,0 +1,60 @@
+package gov.nist.hla.parity.paritysystem;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.paritytrading.parity.match.OrderBook;
+import com.paritytrading.parity.match.OrderBookListener;
+import com.paritytrading.parity.match.Side;
+
+import gov.nist.hla.parity.paritysystem.exception.DuplicateInstrumentName;
+import gov.nist.hla.parity.paritysystem.rti.CancelOrder;
+import gov.nist.hla.parity.paritysystem.rti.EnterOrder;
+
+public class OrderBooks implements OrderBookListener {
+    private final static Logger log = LogManager.getLogger();
+
+    private Map<String, Instrument> instruments = new HashMap<String, Instrument>();
+
+    private Map<String, OrderBook> books = new HashMap<String, OrderBook>();
+
+    public OrderBooks(List<Instrument> instruments) {
+        for (Instrument instrument : instruments) {
+            final String instrumentName = instrument.getInstrumentName();
+            
+            if (this.instruments.put(instrumentName, instrument) != null) {
+                log.error("multiple instruments with the name {}", instrumentName);
+                throw new DuplicateInstrumentName(instrumentName);
+            }
+            log.info("new instrument {}", instrument.toString());
+            
+            books.put(instrumentName, new OrderBook(this));
+            log.debug("created order book for {}", instrumentName);
+        }
+    }
+    
+    void enterOrder(EnterOrder message) {
+    }
+    
+    void cancelOrder(CancelOrder message) {
+    }
+    
+    @Override
+    public void match(long restingOrderId, long incomingOrderId, Side incomingSide, long price, long executedQuantity, long remainingQuantity) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void add(long orderId, Side side, long price, long size) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void cancel(long orderId, long canceledQuantity, long remainingQuantity) {
+        // TODO Auto-generated method stub
+    }
+}
--- src/tesp/IEEE8500/parity/ParityMarket_generated/ParitySystem/src/main/java/gov/nist/hla/parity/paritysystem/ParitySystem.java
+++ src/tesp/IEEE8500/parity/ParityMarket_generated/ParitySystem/src/main/java/gov/nist/hla/parity/paritysystem/ParitySystem.java
@@ -1,41 +1,25 @@
 package gov.nist.hla.parity.paritysystem;
 
-import gov.nist.hla.parity.paritysystem.exception.DuplicateInstrumentName;
-import gov.nist.hla.parity.paritysystem.rti.*;
-
 import org.cpswt.config.FederateConfigParser;
 import org.cpswt.hla.InteractionRoot;
 import org.cpswt.hla.base.AdvanceTimeRequest;
 
-import java.util.HashMap;
-import java.util.Map;
+import gov.nist.hla.parity.paritysystem.rti.CancelOrder;
+import gov.nist.hla.parity.paritysystem.rti.EnterOrder;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-// CodeGeneratedName is used if necessary. Otherwise, name is used.
-// The following interaction classes have both CodeGeneratedName and name.
-// CodeGeneratedName = OrderCanceledReport   name = OrderCanceled
-
-
-// Define the ParitySystem type of federate for the federation.
 
 public class ParitySystem extends ParitySystemBase {
     private final static Logger log = LogManager.getLogger();
 
     private double currentTime = 0;
     
-    private Map<String, Instrument> instruments = new HashMap<String, Instrument>();
-
+    private OrderBooks parity;
+    
     public ParitySystem(ParitySystemConfig params) throws Exception {
         super(params);
-        
-        for (Instrument instrument : params.instruments) {
-            if (instruments.put(instrument.getInstrumentName(), instrument) != null) {
-                log.error("multiple instruments with the name {}", instrument.getInstrumentName());
-                throw new DuplicateInstrumentName(instrument.getInstrumentName());
-            }
-        }
+        parity = new OrderBooks(params.instruments);
     }
 
     private void checkReceivedSubscriptions() {
@@ -95,114 +79,6 @@ public class ParitySystem extends ParitySystemBase {
             // time step below                                        //
             ////////////////////////////////////////////////////////////
 
-            // Set the interaction's parameters.
-            //
-            //    Trade trade = create_Trade();
-            //    trade.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    trade.set_federateFilter( < YOUR VALUE HERE > );
-            //    trade.set_incomingOrderNumber( < YOUR VALUE HERE > );
-            //    trade.set_matchNumber( < YOUR VALUE HERE > );
-            //    trade.set_messageType( < YOUR VALUE HERE > );
-            //    trade.set_originFed( < YOUR VALUE HERE > );
-            //    trade.set_quantity( < YOUR VALUE HERE > );
-            //    trade.set_restingOrderNumber( < YOUR VALUE HERE > );
-            //    trade.set_sourceFed( < YOUR VALUE HERE > );
-            //    trade.set_timeStamp( < YOUR VALUE HERE > );
-            //    trade.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    Version version = create_Version();
-            //    version.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    version.set_federateFilter( < YOUR VALUE HERE > );
-            //    version.set_messageType( < YOUR VALUE HERE > );
-            //    version.set_originFed( < YOUR VALUE HERE > );
-            //    version.set_sourceFed( < YOUR VALUE HERE > );
-            //    version.set_version( < YOUR VALUE HERE > );
-            //    version.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderAccepted orderAccepted = create_OrderAccepted();
-            //    orderAccepted.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderAccepted.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderAccepted.set_instrument( < YOUR VALUE HERE > );
-            //    orderAccepted.set_messageType( < YOUR VALUE HERE > );
-            //    orderAccepted.set_orderId( < YOUR VALUE HERE > );
-            //    orderAccepted.set_orderNumber( < YOUR VALUE HERE > );
-            //    orderAccepted.set_originFed( < YOUR VALUE HERE > );
-            //    orderAccepted.set_price( < YOUR VALUE HERE > );
-            //    orderAccepted.set_quantity( < YOUR VALUE HERE > );
-            //    orderAccepted.set_side( < YOUR VALUE HERE > );
-            //    orderAccepted.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderAccepted.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderAccepted.set_userName( < YOUR VALUE HERE > );
-            //    orderAccepted.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderCanceled orderCanceled = create_OrderCanceled();
-            //    orderCanceled.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderCanceled.set_canceledQuantity( < YOUR VALUE HERE > );
-            //    orderCanceled.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderCanceled.set_messageType( < YOUR VALUE HERE > );
-            //    orderCanceled.set_orderId( < YOUR VALUE HERE > );
-            //    orderCanceled.set_originFed( < YOUR VALUE HERE > );
-            //    orderCanceled.set_reason( < YOUR VALUE HERE > );
-            //    orderCanceled.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderCanceled.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderCanceled.set_userName( < YOUR VALUE HERE > );
-            //    orderCanceled.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderExecuted orderExecuted = create_OrderExecuted();
-            //    orderExecuted.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderExecuted.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderExecuted.set_liquidityFlag( < YOUR VALUE HERE > );
-            //    orderExecuted.set_matchNumber( < YOUR VALUE HERE > );
-            //    orderExecuted.set_messageType( < YOUR VALUE HERE > );
-            //    orderExecuted.set_orderId( < YOUR VALUE HERE > );
-            //    orderExecuted.set_originFed( < YOUR VALUE HERE > );
-            //    orderExecuted.set_price( < YOUR VALUE HERE > );
-            //    orderExecuted.set_quantity( < YOUR VALUE HERE > );
-            //    orderExecuted.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderExecuted.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderExecuted.set_userName( < YOUR VALUE HERE > );
-            //    orderExecuted.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderRejected orderRejected = create_OrderRejected();
-            //    orderRejected.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderRejected.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderRejected.set_messageType( < YOUR VALUE HERE > );
-            //    orderRejected.set_orderId( < YOUR VALUE HERE > );
-            //    orderRejected.set_originFed( < YOUR VALUE HERE > );
-            //    orderRejected.set_reason( < YOUR VALUE HERE > );
-            //    orderRejected.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderRejected.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderRejected.set_userName( < YOUR VALUE HERE > );
-            //    orderRejected.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderEntered orderEntered = create_OrderEntered();
-            //    orderEntered.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderEntered.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderEntered.set_instrument( < YOUR VALUE HERE > );
-            //    orderEntered.set_messageType( < YOUR VALUE HERE > );
-            //    orderEntered.set_orderNumber( < YOUR VALUE HERE > );
-            //    orderEntered.set_originFed( < YOUR VALUE HERE > );
-            //    orderEntered.set_price( < YOUR VALUE HERE > );
-            //    orderEntered.set_quantity( < YOUR VALUE HERE > );
-            //    orderEntered.set_side( < YOUR VALUE HERE > );
-            //    orderEntered.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderEntered.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderEntered.set_userName( < YOUR VALUE HERE > );
-            //    orderEntered.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderAdded orderAdded = create_OrderAdded();
-            //    orderAdded.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderAdded.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderAdded.set_messageType( < YOUR VALUE HERE > );
-            //    orderAdded.set_orderNumber( < YOUR VALUE HERE > );
-            //    orderAdded.set_originFed( < YOUR VALUE HERE > );
-            //    orderAdded.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderAdded.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderAdded.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //    OrderCanceledReport orderCanceledReport = create_OrderCanceledReport();
-            //    orderCanceledReport.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_canceledQuantity( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_federateFilter( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_messageType( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_orderNumber( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_originFed( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_sourceFed( < YOUR VALUE HERE > );
-            //    orderCanceledReport.set_timeStamp( < YOUR VALUE HERE > );
-            //    orderCanceledReport.sendInteraction(getLRC(), currentTime + getLookAhead());
-
             checkReceivedSubscriptions();
 
             ////////////////////////////////////////////////////////////////////
@@ -228,15 +104,11 @@ public class ParitySystem extends ParitySystemBase {
     }
 
     private void handleInteractionClass(CancelOrder interaction) {
-        ///////////////////////////////////////////////////////////////
-        // TODO implement how to handle reception of the interaction //
-        ///////////////////////////////////////////////////////////////
+        parity.cancelOrder(interaction);
     }
 
     private void handleInteractionClass(EnterOrder interaction) {
-        ///////////////////////////////////////////////////////////////
-        // TODO implement how to handle reception of the interaction //
-        ///////////////////////////////////////////////////////////////
+        parity.enterOrder(interaction);
     }
 
     public static void main(String[] args) {
