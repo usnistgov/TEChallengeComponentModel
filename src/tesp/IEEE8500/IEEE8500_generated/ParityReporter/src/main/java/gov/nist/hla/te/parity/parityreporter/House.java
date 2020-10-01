8a10c8ea2b66cf5bb8c3d960f64e635485d4436f
Thomas Roth <thomas.roth@nist.gov>
2020-10-01 04:53:20 +0000

    fixed bill metric

--- src/tesp/IEEE8500/IEEE8500_generated/ParityReporter/src/main/java/gov/nist/hla/te/parity/parityreporter/House.java
+++ src/tesp/IEEE8500/IEEE8500_generated/ParityReporter/src/main/java/gov/nist/hla/te/parity/parityreporter/House.java
@@ -1,6 +1,8 @@
 package gov.nist.hla.te.parity.parityreporter;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 
@@ -32,18 +34,21 @@ public class House {
         this.name = name;
     }
     
-    public void update(long unixTime, long periodInSeconds) {
+    public void update(long unixTime, long periodInSeconds) { // 300
         log.trace("update at t={}", unixTime, periodInSeconds);
         
         update_trade_index(unixTime);
         
+        double energy_diff = measured_real_energy - measured_real_energy_prev;
         
-        double average_real_power = (measured_real_energy - measured_real_energy_prev) / periodInSeconds; // this period needs to match the others (5min)
-        double bill_increase = 0;
-        //  compute bill increment / store total
-        //      bill = sum_{24hr}(price_t * quantity_t + sum_{1hr}(price_now * (meter_{5min} - quantity_t/12)))
+        // bill_increase = sum_{24hr}(price_t * quantity_t + sum_{1hr}(price_now * (meter_{5min} - quantity_t/12)))
+        double transaction_amount = get_price() * get_quantity() / (3600 / periodInSeconds);
+        double real_time_adjustment = real_time_price * (energy_diff - get_quantity() / (3600 / periodInSeconds));
+        double bill_increase = transaction_amount + real_time_adjustment;
         
-        log.info("House {} Total Bill = {} (+{})", name, total_bill, bill_increase);
+        total_bill = total_bill + bill_increase;
+        log.debug("average_real_power={} transaction_amount={} real_time_adjustment={}", energy_diff, transaction_amount, real_time_adjustment);
+        log.info("House {} Total Bill = {} ({})", name, total_bill, bill_increase);
     }
     
     // assume unixTime is monotonically increasing
@@ -57,23 +62,26 @@ public class House {
         }
         
         String tradeTimeText = trades.get(trade_index).getStartTime();
-        Instant tradeTime = Instant.parse(tradeTimeText);
+        Instant tradeTime = LocalDateTime.parse(tradeTimeText).toInstant(ZoneOffset.UTC); // ???
+        log.debug("{} {} {}", currentTime, tradeTimeText, tradeTime);
         
         if (currentTime.isBefore(tradeTime)) {
             log.error("no trade data for {} before {}", name, unixTime);
             return; // missed edge case: this exit condition + update called to compute bill
         }
         
+        int next_index = trade_index;
         while (tradeTime.isBefore(currentTime)) {
-            trade_index++;
-            if (trade_index >= trades.size()) {
+            next_index++;
+            if (next_index >= trades.size()) {
                 log.error("no trade data for {} after {}", name, unixTime);
                 return;
             }
             
-            tradeTimeText = trades.get(trade_index).getStartTime();
-            tradeTime = Instant.parse(tradeTimeText);
+            tradeTimeText = trades.get(next_index).getStartTime();
+            tradeTime = LocalDateTime.parse(tradeTimeText).toInstant(ZoneOffset.UTC); // ???
         }
+        trade_index = next_index - 1;
         
         log.debug("trade_index {} -> {} at t={}", trade_index, tradeTime, currentTime);
     }
@@ -97,18 +105,13 @@ public class House {
         this.real_time_price = value;
     }
     
-    // assume measured_real_energy and real_time_price set
-    public void calculate_bill() {
-        log.trace("calculate_bill");
-    }
-    
     // assume 24 trades with 60 minute durations
     // TODO: match quantity units to measured_real_energy units
     public void add_trade(Trade trade) {
         log.trace("add_trade {}", trade.toString());
         final int time_zone_adjustment = 0; // what should this be?
         
-        Instant time = Instant.parse(trade.getStartTime());
+        LocalDateTime time = LocalDateTime.parse(trade.getStartTime());
         int hour = time.get(ChronoField.HOUR_OF_DAY);
         hour = (hour + time_zone_adjustment) % 24;
         
--- src/tesp/IEEE8500/IEEE8500_generated/ParityReporter/src/main/java/gov/nist/hla/te/parity/parityreporter/ParityReporter.java
+++ src/tesp/IEEE8500/IEEE8500_generated/ParityReporter/src/main/java/gov/nist/hla/te/parity/parityreporter/ParityReporter.java
@@ -2,6 +2,9 @@ package gov.nist.hla.te.parity.parityreporter;
 
 import gov.nist.hla.te.parity.parityreporter.exception.InvalidTransactionException;
 import gov.nist.hla.te.parity.parityreporter.rti.*;
+import hla.rti.ConcurrentAccessAttempted;
+import hla.rti.FederateNotExecutionMember;
+import hla.rti.RTIinternalError;
 
 import org.cpswt.config.FederateConfig;
 import org.cpswt.config.FederateConfigParser;
@@ -9,7 +12,10 @@ import org.cpswt.hla.InteractionRoot;
 import org.cpswt.hla.ObjectRoot;
 import org.cpswt.hla.base.AdvanceTimeRequest;
 import org.cpswt.hla.base.ObjectReflector;
+import org.cpswt.utils.CpswtUtils;
 
+import java.time.Instant;
+import java.time.temporal.ChronoField;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +27,17 @@ public class ParityReporter extends ParityReporterBase {
     
     private Map<Long, Trade> trades = new HashMap<Long, Trade>();
 
+    private Map<String, House> houses = new HashMap<String, House>();
+    
     private double currentTime = 0;
+    
+    private long secondsPerStep;
+    
+    private long unixTime;
+    
+    private boolean receivedSimTime = false;
+    
+    private boolean receivedTrades = false;
 
     public ParityReporter(FederateConfig params) throws Exception {
         super(params);
@@ -69,6 +85,17 @@ public class ParityReporter extends ParityReporterBase {
             readyToPopulate();
             log.info("...synchronized on readyToPopulate");
         }
+        
+        while (!receivedSimTime) {
+            log.info("waiting to receive SimTime...");
+            synchronized (lrc) {
+                lrc.tick();
+            }
+            checkReceivedSubscriptions();
+            if (!receivedSimTime) {
+                CpswtUtils.sleep(1000);
+            }
+        }
 
         if(!super.isLateJoiner()) {
             log.info("waiting on readyToRun...");
@@ -82,15 +109,33 @@ public class ParityReporter extends ParityReporterBase {
         while (!exitCondition) {
             atr.requestSyncStart();
             enteredTimeGrantedState();
-
+            
             checkReceivedSubscriptions();
-
+            
+            if (receivedTrades) { // assume one bulk receive
+                for (House house : houses.values()) {
+                    house.clear_trades();
+                }
+                receivedTrades = false;
+            }
+            
             // TODO: external (to GLD) real time price feed
             // TODO: somehow have reports (like daily) for the bill data
+            long integerTime = (long) this.getCurrentTime();
+            long updatePeriod = 300 / secondsPerStep; // can lead to errors
             
-            // receive SimTime to get the seconds/logical time
-            // receive meter for measured_real_energy per meter (NEED TO ASSOCIATE METER WITH HOUSE!!!) meter_house naming scheme
-
+            // offset by 1 to process after receiving the data from FileReader
+            // delay 10 (5 minutes from previous day; 5 minutes until first bill)
+            if (integerTime > 10 && integerTime % updatePeriod == 1) {
+                log.info("bill update at t={}", this.getCurrentTime());
+                
+                for (House house : houses.values()) {
+                    house.update(unixTime, 300); // nice magic number
+                }
+            }
+            
+            unixTime = unixTime + secondsPerStep; // right location ?
+            
             if (!exitCondition) {
                 currentTime += super.getStepSize();
                 AdvanceTimeRequest newATR =
@@ -106,9 +151,16 @@ public class ParityReporter extends ParityReporterBase {
     }
     
     private void handleInteractionClass(SimTime interaction) {
-        ///////////////////////////////////////////////////////////////
-        // TODO implement how to handle reception of the interaction //
-        ///////////////////////////////////////////////////////////////
+        if (receivedSimTime) {
+            // replace the old values with the latest received version
+            log.warn("received duplicate SimTime");
+        } else {
+            log.info("received SimTime");
+        }
+        
+        unixTime = interaction.get_unixTimeStart();
+        secondsPerStep = (long) interaction.get_timeScale(); // may lead to errors
+        receivedSimTime = true;
     }
     
     private void handleInteractionClass(Transaction interaction) {
@@ -122,6 +174,15 @@ public class ParityReporter extends ParityReporterBase {
                 Trade trade = trades.get(matchNumber);
                 trade.update(interaction);
                 log.info(trade.toString());
+                
+                // ignore seller (assumed utility)
+                String houseName = trade.getBuyer();
+                if (!houses.containsKey(houseName)) {
+                    houses.put(houseName, new House(houseName));
+                    log.info("created new house {}", houseName);
+                }
+                houses.get(houseName).add_trade(trade);
+                receivedTrades = true;
             }
         } catch (InvalidTransactionException e) {
             log.error("failed to process transaction: {}", e.getMessage());
@@ -129,9 +190,17 @@ public class ParityReporter extends ParityReporterBase {
     }
     
     private void handleObjectClass(Meter object) {
-        //////////////////////////////////////////////////////////
-        // TODO implement how to handle reception of the object //
-        //////////////////////////////////////////////////////////
+        String houseName = object.get_name() + "_house";
+        
+        House house = houses.get(houseName);
+        if (house == null) {
+            log.error("cannot find house {}", houseName);
+            return;
+        }
+        
+        house.set_measured_real_energy(object.get_measured_real_energy());
+        house.set_real_time_price(object.get_price());
+        log.debug("updated house {}", houseName);
     }
 
     public static void main(String[] args) {
