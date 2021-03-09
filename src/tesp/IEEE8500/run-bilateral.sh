67b49a7b2a81a1abf5f7aab71d9b370dd19c03b6
Thomas Roth <thomas.roth@nist.gov>
2021-03-09 23:47:42 +0000

    scripts to run new cases

---
+++ src/tesp/IEEE8500/run-bilateral.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+root_directory=`pwd`
+logs_directory=$root_directory/logs
+code_directory=`realpath IEEE8500_generated`
+
+fedmgr_host=127.0.0.1
+fedmgr_port=8083
+
+timestamp=`date +"%F_%T"`
+
+function getNumberJoined {
+    if (( $# != 1 ))
+    then
+        echo bad syntax: getNumberJoined federateType
+        exit 1
+    fi
+    federateList=$(curl -s -X GET http://$fedmgr_host:$fedmgr_port/federates -H "Content-Type: application/json")
+    # JSON Query:
+    #   .[] = process all values in the input object
+    #   select(...) = exclude entries for resigned federates (resignTime defined) and federates that are not the desired TYPE
+    #   enclosing [ ] = combine the result from the above queries into a single JSON array
+    #   length = count the number of elements in the combined array
+    echo $federateList | jq --arg TYPE "$1" '[.[] | select(.resignTime == null and .federateType == $TYPE)] | length'
+}
+
+function waitUntilJoined {
+    if (( $# != 2 ))
+    then
+        echo bad syntax: waitUntilJoined federateType expectedNumber
+        exit 1
+    fi
+    federateType=$1
+    expectedNumber=$2
+
+    if (( $expectedNumber < 1 ))
+    then
+        echo "illegal argument: expectedNumber of federates cannot be $expectedNumber"
+        exit 1
+    fi
+
+    printf "Waiting for $expectedNumber instances of $federateType to join.."
+    while (( $(getNumberJoined $federateType) != $expectedNumber))
+    do
+        printf "."
+        sleep 5
+    done
+    printf "\n"
+}
+
+nc -z $fedmgr_host $fedmgr_port
+if [ $? -eq 0 ]; then
+    echo Cannot start the federation manager on port $fedmgr_port
+    exit 1
+fi
+
+if [ ! -d $logs_directory ]; then
+    echo Creating the $logs_directory directory
+    mkdir $logs_directory
+fi
+
+# run the federation manager
+cd $root_directory/IEEE8500_deployment
+xterm -fg white -bg black -l -lf $logs_directory/federation-manager-${timestamp}.log -T "Federation Manager" -geometry 140x40+0+30 -e "export CPSWT_ROOT=`pwd` && mvn exec:java -P FederationManagerBilateral" &
+
+printf "Waiting for the federation manager to come online.."
+until $(curl -o /dev/null -s -f -X GET http://$fedmgr_host:$fedmgr_port/fedmgr); do
+    printf "."
+    sleep 5
+done
+printf "\n"
+
+curl -o /dev/null -s -X POST http://$fedmgr_host:$fedmgr_port/fedmgr --data '{"action": "START"}' -H "Content-Type: application/json"
+
+# run the other federates
+cd $code_directory/SimulationTime
+xterm -fg yellow -bg black -l -lf $logs_directory/SimulationTime-${timestamp}.log -T "SimulationTime" -geometry 140x40+30+60 -e "sh run.sh" &
+waitUntilJoined SimulationTime 1
+
+cd $code_directory/GridModel
+xterm -fg green -bg black -l -lf $logs_directory/GridModel-${timestamp}.log -T "GridModel" -geometry 140x40+60+90 -e "sh run.sh" &
+waitUntilJoined GridModel 1
+
+cd $code_directory/Controller
+xterm -fg cyan -bg black -l -lf $logs_directory/Controller-${timestamp}.log -T "Controller" -geometry 140x40+90+120 -e "sh run.sh" &
+waitUntilJoined Controller 1
+
+# terminate the simulation
+read -n 1 -r -s -p "Press any key to terminate the federation execution..."
+printf "\n"
+
+curl -o /dev/null -s -X POST http://$fedmgr_host:$fedmgr_port/fedmgr --data '{"action": "TERMINATE"}' -H "Content-Type: application/json"
+
+printf "Waiting for the federation manager to terminate.."
+while $(curl -o /dev/null -s -f -X GET http://$fedmgr_host:$fedmgr_port/fedmgr); do
+    printf "."
+    sleep 5
+done
+printf "\n"
+
