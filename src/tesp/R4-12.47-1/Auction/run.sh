26c2f9379225d264e33ca8e3264f7368c9691b13
Thomas Roth <thomas.roth@nist.gov>
2022-02-24 04:49:07 +0000

    fixes for new Auction

--- src/tesp/R4-12.47-1/Auction/run.sh
+++ src/tesp/R4-12.47-1/Auction/run.sh
@@ -5,5 +5,5 @@ if [ $# -eq 1 ] && [ -f $1 ]; then
 fi
 
 echo using the file $configFile
-java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=conf/log4j2.xml -jar target/Auction-0.1.0-SNAPSHOT.jar -configFile $configFile
+java -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=conf/log4j2.xml -jar Auction-0.1.0-SNAPSHOT.jar -configFile $configFile
 
